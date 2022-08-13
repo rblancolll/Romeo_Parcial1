6e36e7bc676f91fd3e3a3baedb8348db3deea98a
rblancolll <rblancol@miumg.edu.gt>
2022-08-13 09:54:41 -0600

    Se creo el Controller

---
+++ src/java/Clases/LibroController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author JP
+ */
+public class LibroController {
+      Libro[] tablaLibro;
+    int indiceArray;
+    
+    public LibroController(){
+        this.tablaLibro = new Libro[100];
+        this.indiceArray=0;
+    }
+    
+     public void guardarLibro(Libro libro){
+        this.tablaLibro[this.indiceArray]=libro;  
+        this.indiceArray=this.indiceArray+1;
+    }
+    
+    public Libro[] getLibros(){
+        return this.tablaLibro;
+    }
+    
+}
